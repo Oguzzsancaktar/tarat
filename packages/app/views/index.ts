825c9e87b192f43bc46dc87d98a559a48d24c37f
Subasifinans33 <subasiberk214@gmail.com>
2023-10-27 17:28:36 +0300

    27/Creation of profile page desgin - 29 Edit profile form creation

--- packages/app/views/index.ts
+++ packages/app/views/index.ts
@@ -4,4 +4,5 @@ export { default as MailRegisterScreen } from './MailRegisterScreen';
 export { default as MailLoginScreen } from './MailLoginScreen'
 export { default as RegisterScreen } from './RegisterScreen';
 export { default as WelcomeScreen } from './WelcomeScreen';
-export { default as SettingsScreen } from './SettingsScreen'
+export { default as SettingsScreen } from './SettingsScreen';
+export { default as EditProfileScreen } from './EditProfileScreen';
