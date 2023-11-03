a70a8f00a09801b499b8cf8616bb9536528b7453
oguzzsancaktar <oguztahasancaktar@gmail.com>
2023-11-03 15:31:51 +0300

    Fixed;

--- packages/common/views/WelcomeScreen.tsx
+++ packages/common/views/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, Paragraph, , View, H3 } from '@packages/ui'
+import { Image, Paragraph, Stack, View, H3 } from '@packages/ui'
 import { useAppImages } from '@packages/common/hooks'
 import { useLink } from 'solito/link'
 import { Button } from '@packages/ui/src/components'
@@ -18,31 +18,31 @@ export default function WelcomeScreen() {
 
   return (
     <ScreenContainer>
-      <YStack maw={400} alignSelf="center" jc="space-between" ai="center" py="$10" px="$4">
-        <YStack maw={600}>
+      <Stack maw={400} alignSelf="center" jc="space-between" ai="center" py="$10" px="$4">
+        <Stack maw={600}>
           {/*Images */}
           <View jc={'center'} ai={'center'}>
             <Image mt={100} w={300} source={appImages.thumbnail} />
           </View>
           {/*LOGO*/}
-          <YStack jc={'center'} ai={'center'}>
+          <Stack jc={'center'} ai={'center'}>
             <H3>TARAT</H3>
-          </YStack>
+          </Stack>
           {/*Slogan */}
-          <YStack jc={'center'} ai={'center'}>
+          <Stack jc={'center'} ai={'center'}>
             <Paragraph>Sen de TARAT ile nesnelerin hakkında bilgi ver.</Paragraph>
-          </YStack>
+          </Stack>
           {/*Button */}
-          <YStack jc={'center'} ai={'center'}>
+          <Stack jc={'center'} ai={'center'}>
             <Button background="orange" {...loginLinkProps} size="large">
               Giriş Yap
             </Button>
             <Button {...registerLimkProps} size="large" background="white">
               Kayıt Ol a
             </Button>
-          </YStack>
-        </YStack>
-      </YStack>
+          </Stack>
+        </Stack>
+      </Stack>
     </ScreenContainer>
   )
 }
