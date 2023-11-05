d0bf0279c8e38a25e0ebb28ef374ca4a305744ca
Subasifinans33 <subasiberk214@gmail.com>
2023-11-05 19:14:52 +0300

    Validation Messages Added

--- packages/common/constants/VALIDATION_MESSAGES.ts
+++ packages/common/constants/VALIDATION_MESSAGES.ts
@@ -1,5 +1,10 @@
 const VALIDATION_MESSAGES = {
-  usernameRequired: 'Kullanıcı Adı Gereklidir'
+  usernameRequired: 'Kullanıcı Adı Gerekli',
+  emailRequired: 'E-posta Adresi Gerekli',
+  phoneRequired: 'Telefon Numarası Gerekli',
+  passwordRequired: 'Şifre Gerekli',
+  passwordConfirmRequired: 'Şifrenizi Onaylamanız Gerekli',
+  passwordMatchRequired: 'Şifreler Eşleşmiyor',
 }
 
 export default VALIDATION_MESSAGES;
