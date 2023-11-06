fff109a6c90b3becf7beae68abba5d7f390151bc
Subasifinans33 <subasiberk214@gmail.com>
2023-11-06 19:06:00 +0300

    sign index folder fixed

--- packages/common/views/WelcomeScreen.tsx
+++ packages/common/views/WelcomeScreen.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { Image, Paragraph, View, H3, YStack } from '@packages/ui'
 import { useAppImages } from '@packages/common/hooks'
 import { useLink } from 'solito/link'
-import { Button, XStackFullW, YStackFullW } from '@packages/ui/src/components'
+import { Button, YStackFullW } from '@packages/ui/src/components'
 import { ScreenContainer } from '../components/containers'
-import { BottomTabNavigator } from '../components/sign/bottom/BottomBar'
 
 export default function WelcomeScreen() {
   const appImages = useAppImages()
@@ -43,7 +42,6 @@ export default function WelcomeScreen() {
             Kayıt Ol
           </Button>
         </YStackFullW>
-        <BottomTabNavigator />
       </YStack>
     </ScreenContainer>
   )
